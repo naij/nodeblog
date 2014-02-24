8640a2132bec900e58a2f83271e0a0411405ac08
陆议 <fruit.wj@taobao.com>
2014-02-24 12:52:49 +0800

    图片上传功能更新

--- public/app/views/manage/picture/add.js
+++ public/app/views/manage/picture/add.js
@@ -16,49 +16,104 @@ KISSY.add("app/views/manage/picture/add", function (S, View, MM, VOM, Router, No
         components: function () {
             
         },
-        picPreview: function (file, index) {
+        picPreview: function (files) {
             var me = this;
             var pagelet = me.getManaged('pagelet');
+            var filesLength = files.length;
+            var pics = [];
 
-            if (file) {
-                var fileSize = 0;
-                if (file.size > 1024 * 1024) {
-                    fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
-                } else {
-                    fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
+            for (var i = 0; i < filesLength; i ++) {
+                var file = files[i];
+                var fileObj = {};
+
+                if (file) {
+                    var fileSize = 0;
+                    if (file.size > 1024 * 1024) {
+                        fileObj.fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
+                    } else {
+                        fileObj.fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
+                    }
+                }
+
+                if (file.type.indexOf("image") == 0) {
+                    var reader = new FileReader();
+                    reader.readAsDataURL(file);
+
+                    (function (fileObj, index) {
+                        reader.onload = function (e) {
+                            fileObj.filePath = e.target.result;
+                            pics.push(fileObj);
+
+                            if (index == filesLength -1) {
+                                pagelet.setChunkData('pics', pics);
+                            }
+                        };
+                    }(fileObj, i));
                 }
             }
+        },
+        uploadFile: function (files) {
+            var me = this;
 
-            if (file.type.indexOf("image") == 0) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    pagelet.setChunkData('pics', [{picPath: e.target.result}], {renderType: 'append'});
-                };
-                reader.readAsDataURL(file);
+            //创建FormData()对象
+            var fd = new FormData();
+            fd.append("author", "Shiv Kumar");
+            fd.append("name", "Html 5 File API/FormData");
+            //文件对象 file            
+            fd.append("fileToUpload", files[0]);
+            //准备使用ajax上传
+            var xhr = new XMLHttpRequest();
+            //进度条            
+            xhr.upload.addEventListener("progress", me.uploadProgress, false);
+            //下载
+            xhr.addEventListener("load", me.uploadComplete, false);
+            //错误信息
+            xhr.addEventListener("error", me.uploadFailed, false);
+            //取消，此功能没有做
+            xhr.addEventListener("abort", me.uploadCanceled, false);
+            //上传
+            xhr.open("POST", "/manage/pictureAdd");
+            //发送
+            xhr.send(fd);
+        },
+        uploadProgress: function (evt) {
+            if (evt.lengthComputable) {
+                //evt.loaded：文件上传的大小   
+                //evt.total：文件总的大小
+                var percentComplete = Math.round((evt.loaded) * 100 / evt.total);
+                //加载进度条，同时显示信息
+                S.log(percentComplete.toString() + '%');
+                //如果上传的结果是100时才让加载下一个文件。如果不够100会继续上传原来的文档。
+                
+                if (percentComplete == 100) {
+                    // a++;
+                    // //加载下一个文档
+                    // uploadFile(file[a])
+                }
             }
+        },
+        uploadComplete: function (evt) {
+
+        },
+        uploadFailed: function (evt) {
+
+        },
+        uploadCanceled: function (evt) {
+
         },
         'fileChange<change>': function (e) {
             e.halt();
             var me = this;
-            var files = $('#' + e.currentId)[0].files;
+            var files = $('#J_upload')[0].files;
 
-            S.each(files, function (v, k) {
-                me.picPreview(v, k);
-            });
+            me.picPreview(files);
         },
-        'submit<click>': function (e) {
+        'upload<click>': function (e) {
             e.halt();
             var me = this;
-            var editor = me.getManaged('editor');
-            editor.codemirror.save();
-            var formData = S.unparam( S.IO.serialize('#addForm'));
+            var files = $('#J_upload')[0].files;
 
-            me.manage(MM.fetchAll([{
-                name: "article_add",
-                postParams: formData
-            }], function (errs, MesModel) {
-                me.navigate('/manage/article/list');
-            }));
+            me.uploadFile(files);
         }
     });
 },{
