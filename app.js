22d3f7591f2bc2e599a8e473a3d1408ab2544efd
陆议 <fruit.wj@taobao.com>
2014-09-12 11:17:29 +0800

    模板改成jade

--- app.js
+++ app.js
@@ -1,52 +1,34 @@
 var express = require('express');
-var ejs = require('ejs');
-var fs = require('fs');
+var jade = require('jade');
 var path = require('path');
+var config = require('config');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var serveStatic = require('serve-static');
 var routes = require('./routes');
-var manage = require('./controllers/manage');
-var config = require('./config').config;
 var app = express();
-var pidfile = path.join(__dirname, 'app.pid');
 
-app.configure(function () {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'html');
-    app.engine('html', ejs.renderFile);
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.cookieParser());
-    app.use(express.session({
-        secret: config.session_secret
-    }));
-    app.use(function (req, res, next) {
-        res.locals.config = config;
-        next();
-    });
-    app.use(app.router);
-
-    fs.writeFileSync(pidfile, process.pid);
-});
-
-app.configure('development', function () {
-    app.use(express.errorHandler({ 
-        dumpExceptions: true, 
-        showStack: true 
-    }));
-});
-
-app.configure('production', function () {
-    app.use(express.errorHandler());
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({
+    secret: config.session_secret
+}));
+app.use(function (req, res, next) {
+    res.locals.config = config;
+    next();
 });
 
 routes(app);
 
-app.listen(3000);
-
-// 退出进程
-process.on('SIGTERM', function () {
-    if (fs.existsSync(pidfile)) {
-        fs.unlinkSync(pidfile);
-    }
-    process.exit(0);
-});
+app.listen(app.get('port'));
