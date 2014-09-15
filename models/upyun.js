a434959a8f9946ce063609240e5b13d296176c5f
陆议 <fruit.wj@taobao.com>
2014-09-15 22:54:52 +0800

    upyun sdk up

--- models/upyun.js
+++ models/upyun.js
@@ -1,10 +1,10 @@
 var config = require('config');
-var UPYun = require('../libs/upyun').UPYun;
+var upyun = require('upyun');
 var backname = config.upyun_buckname;
 var username = process.env.UPYUN_USERNAME;
 var password = process.env.UPYUN_PASSWORD;
 
 // 初始化又拍云服务
-var upyun = new UPYun(backname, username, password);
+var upyunClient = new upyun(backname, username, password);
 
-exports.upyun = upyun;
+exports.upyun = upyunClient;
